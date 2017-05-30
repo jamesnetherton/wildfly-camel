f79f2f70c8f9db9e6929a1af7a6adf234ee149ab
James Netherton <jamesnetherton@gmail.com>
2017-05-30 10:56:24 +0100

    Remove references to resteasy client

--- itests/standalone/basic/src/test/java/org/wildfly/camel/test/cxf/rs/CXFRSConsumerIntegrationTest.java
+++ itests/standalone/basic/src/test/java/org/wildfly/camel/test/cxf/rs/CXFRSConsumerIntegrationTest.java
@@ -19,10 +19,6 @@
  */
 package org.wildfly.camel.test.cxf.rs;
 
-import java.io.InputStream;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.camel.CamelContext;
@@ -30,13 +26,13 @@ import org.apache.camel.ServiceStatus;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.gravia.resource.ManifestBuilder;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.Asset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.wildfly.camel.test.common.http.HttpRequest;
+import org.wildfly.camel.test.common.http.HttpRequest.HttpResponse;
 import org.wildfly.camel.test.common.types.GreetingService;
 import org.wildfly.extension.camel.CamelAware;
 import org.wildfly.extension.camel.CamelContextRegistry;
@@ -50,38 +46,22 @@ public class CXFRSConsumerIntegrationTest {
 
     @Deployment
     public static JavaArchive deployment() {
-        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "cxfrs-consumer-tests");
-        archive.addClasses(GreetingService.class);
-        archive.addAsResource("cxf/spring/cxfrs-consumer-camel-context.xml", "cxfrs-consumer-camel-context.xml");
-        archive.setManifest(new Asset() {
-            @Override
-            public InputStream openStream() {
-                ManifestBuilder builder = new ManifestBuilder();
-                builder.addManifestHeader("Dependencies", "org.jboss.resteasy.resteasy-jaxrs");
-                return builder.openStream();
-            }
-        });
-        return archive;
+        return ShrinkWrap.create(JavaArchive.class, "cxfrs-consumer-tests")
+            .addClasses(GreetingService.class, HttpRequest.class)
+            .addAsResource("cxf/spring/cxfrs-consumer-camel-context.xml", "cxfrs-consumer-camel-context.xml");
     }
 
     @Test
     public void testCXFConsumer() throws Exception {
-
         CamelContext camelctx = contextRegistry.getCamelContext("cxfrs-undertow");
         Assert.assertNotNull("Expected cxfrs-undertow to not be null", camelctx);
         Assert.assertEquals(ServiceStatus.Started, camelctx.getStatus());
 
-        ClassLoader tccl = Thread.currentThread().getContextClassLoader();
-        try {
-            // The JAXRS Client API needs to see resteasy
-            Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
+        HttpResponse response = HttpRequest.get("http://localhost:8080/cxfrestful/greet/hello/Kermit")
+            .header("Accept", MediaType.APPLICATION_JSON)
+            .getResponse();
 
-            Client client = ClientBuilder.newClient();
-            Object result = client.target("http://localhost:8080/cxfrestful/greet/hello/Kermit").request(MediaType.APPLICATION_JSON).get(String.class);
-            Assert.assertEquals("Hello Kermit", result);
-        } finally {
-            Thread.currentThread().setContextClassLoader(tccl);
-        }
+        Assert.assertEquals("Hello Kermit", response.getBody());
     }
 
 }
